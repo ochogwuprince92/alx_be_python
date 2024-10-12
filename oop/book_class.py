8802615461752b631bee711b5f2272b3348d6e93
ochogwuprince92 <ochogwuprince92@gmail.com>
2024-10-12 22:09:22 +0100

    Add corrected Book class with magic methods to oop directory

--- oop/book_class.py
+++ oop/book_class.py
@@ -23,17 +23,3 @@ class Book:
     def __repr__(self):
         # Return a string that could be used to recreate the Book object
         return f"Book('{self.title}', '{self.author}', {self.year})"
-
-# Create an instance of the Book class for testing purposes
-book1 = Book("1984", "George Orwell", 1949)
-
-# Call the __str__ method to get a user-friendly string representation of the book
-book_str = str(book1)
-
-# Call the __repr__ method to get the official representation of the book
-book_repr = repr(book1)
-
-# Delete the book1 object, which will trigger the __del__ method and print the deletion message
-del book1
-
-(book_str, book_repr)  # Displaying the results of __str__ and __repr__ methods
